2db28435c6e8d0f1ba0fa0cedfe9fdfcdbdf5bb2
Kyle Whittemore <kyle.whittemore@gmail.com>
2019-11-08 22:38:48 -0500

    mysql database connected

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Str@ton523",
+    database: "timer_db"
+})
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: ", err.stack);
+        return;
+    }
+    console.log("connected as id ", connection.threadId);
+})
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,3 +26,10 @@ app.use(express.json());
 app.listen(PORT, function() {
     console.log("Server is running on port: ", PORT)
 });
+
+const queryString = `insert into activities (activity_name) values ("hamming it up") `
+
+connection.query(queryString, (err, result) => {
+    if (err) throw err;
+    console.log(result)
+})
