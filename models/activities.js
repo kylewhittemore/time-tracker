ae3c02e479a37405b7fdeab8eda6583fc043c6ca
Kyle Whittemore <kyle.whittemore@gmail.com>
2019-11-09 20:41:36 -0500

    get all activities route added

--- models/activities.js
+++ models/activities.js
@@ -1,8 +1,17 @@
 const db_connection = require('../config/db_connection')
 
 const activities = {
-    create: function(name, cb) {
-        db_connection.query(`insert into activities (activity_name) values ("${name}");`, (err, result) => {
+    create: function(activity, cb) {
+        let queryString = `insert into activities (activity_name) values ("${activity}");`
+        db_connection.query(queryString, (err, result) => {
+            if (err) throw err;
+            cb(result)
+        })
+    },
+
+    getAll: function(cb) {
+        let queryString = `select * from activities;`
+        db_connection.query(queryString, function(err, result) {
             if (err) throw err;
             cb(result)
         })
